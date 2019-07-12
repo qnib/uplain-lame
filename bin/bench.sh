c23eb9e6edafb8a742b59452c3e91d1d67aab1c5
Christian Kniep <christian@qnib.org>
2019-07-12 15:16:07 +0200

    forgot dollar

--- bin/bench.sh
+++ bin/bench.sh
@@ -4,5 +4,5 @@ for x in $(find /input/ -name "*.wav");do
     start=$(date +%s)
     lame -V0 ${x} $(echo ${x} |sed -e 's/wav$/mp3/' |sed -e 's/input/output/') 1>/dev/null 2>/dev/null
     end=$(date +%s)
-    echo "${x} -> ((${end}-${start})) seconds"
+    echo "${x} -> $((${end}-${start})) seconds"
 done
