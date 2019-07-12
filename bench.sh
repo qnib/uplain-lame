5177c5133af476f149944ceecffa84e35fb8bc5c
Christian Kniep <christian@qnib.org>
2019-07-12 15:39:57 +0200

    add input creation

--- bench.sh
+++ bench.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [[ $(docker volume ls -q|grep -c '^input$') -eq 0 ]];then
+    echo ">> Create input-volume and copy 3600s wav-sample"
+    set -x
+    docker volume create input
+    docker run -ti --rm -v input:/input/ qnib/white-noise:3600s cp /opt/white-3600s.wav /input/white-3600s.wav
+    set +x
+fi
 MARCHS="core2,nehalem,sandybridge,ivybridge,haswell,skylake,znver1"
 for x in $(echo ${MARCHS} |sed -e 's/,/ /g');do
 	printf "%40s: " qnib/uplain-lame:${x}.2019-07-12 
-    docker run -e SKIP_ENTRYPOINTS=true -e QUIET_ENTRYPOINT=true -ti --rm -v /data/input:/input qnib/uplain-lame:${x}.2019-07-12
+    docker run -e SKIP_ENTRYPOINTS=true -e QUIET_ENTRYPOINT=true -ti --rm -v input:/input/ qnib/uplain-lame:${x}.2019-07-12
 done
